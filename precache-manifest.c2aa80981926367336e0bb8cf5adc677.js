52cc8125f9bd78d01b2507abc581f3285bb3abe2
Christiaan Hemerik <Christiaan.Hemerik@thelearningnetwork.nl>
2021-07-23 16:23:19 +0200

    Updates

---
+++ precache-manifest.c2aa80981926367336e0bb8cf5adc677.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7792246d4a8237aed711",
+    "url": "0.4c0850af.iframe.bundle.js"
+  },
+  {
+    "revision": "4ed585c0b5102e2611ee",
+    "url": "4.324e69c6.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.324e69c6.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "013fbabb078cd2be7467",
+    "url": "5.6751407b.iframe.bundle.js"
+  },
+  {
+    "revision": "1b0497b2b103155606fd",
+    "url": "6.e9c4f4dd.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.e9c4f4dd.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c4e88315427f113581a",
+    "url": "7.e1a99765.iframe.bundle.js"
+  },
+  {
+    "revision": "b2b0d65bdb3fdcb15938d90992ce5832",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "766ae5e77915fa503c0a",
+    "url": "main.6c1f486c.iframe.bundle.js"
+  },
+  {
+    "revision": "463dcb2d95216d3ad519",
+    "url": "runtime~main.8f4a6b5a.iframe.bundle.js"
+  },
+  {
+    "revision": "766ae5e77915fa503c0a",
+    "url": "static/css/main.5850987f.chunk.css"
+  },
+  {
+    "revision": "d00a968e744d3b8f01b83f5eba468803",
+    "url": "static/media/app-example-01.d00a968e.png"
+  },
+  {
+    "revision": "773203f87b5ff71f538bddf06a4675c2",
+    "url": "static/media/article-example-01.773203f8.png"
+  },
+  {
+    "revision": "d6661cd7a0e9777c9656",
+    "url": "vendors~main.0bd6dfe0.iframe.bundle.js"
+  },
+  {
+    "revision": "88efc55938e06cdc24cde1623296fb77",
+    "url": "vendors~main.0bd6dfe0.iframe.bundle.js.LICENSE.txt"
+  }
+]);
